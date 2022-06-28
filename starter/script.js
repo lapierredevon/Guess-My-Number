88740728355b596102b518c1d88fb99971bac17c
Devon Lapierre <lapierredevon@gmail.com>
2022-06-28 14:30:05 -0400

    Learning Dom Guess My Number

---
+++ starter/script.js
@@ -0,0 +1,16 @@
+'use strict';
+/*console.log(document.querySelector(".message").textContent);
+
+console.log(document.querySelector(".message").textContent = "Correct Number!")
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 29
+console.log(document.querySelector('.guess').value);*/
+
+document.querySelector(".check").addEventListener('click',function (){
+    console.log(document.querySelector('.guess').value)
+})
+
+
